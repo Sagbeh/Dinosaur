22b3231aa41b14c6d974cd705fbb4406424bdb1a
sagbeh <email@email.com>
2017-02-15 17:28:49 -0600

    Added a dino

---
+++ Dinosaur.java
@@ -0,0 +1,12 @@
+package com.Sam;
+
+/**
+ * Created by samagbeh on 2/15/17.
+ */
+public class Dinosaur {
+
+    public void roar() {
+
+        System.out.println("GRRRRRR!!!");
+    }
+}
---
+++ DinosaurSupervisor.java
@@ -0,0 +1,19 @@
+package com.Sam;
+
+/**
+ * Created by samagbeh on 2/15/17.
+ */
+public class DinosaurSupervisor {
+
+    public static void  main(String[] args) {
+
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+
+        t_rex.roar();
+        velociraptor.roar();
+
+        Dinosaur stegosaurus = new Dinosaur();
+        stegosaurus.roar();
+    }
+}
